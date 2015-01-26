6a4d226cb43fa8886250b682e8ed8bcc51e72831
Yuri Artemev <i@artemeff.com>
2015-01-26 18:04:46 +0300

    add index page

--- src/transee_app.erl
+++ src/transee_app.erl
@@ -3,7 +3,23 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    {ok, _} = cowboy:start_http(http, 100
+        , [ {ip, {127,0,0,1}}, {port, 8000} ]
+        , [ {env, [{dispatch, routes()}]}
+          % {onrequest,  fun on_request/1}
+          % {onresponse, fun on_response/4}
+          ]
+        ),
     transee_sup:start_link().
 
 stop(_State) ->
     ok.
+
+routes() -> cowboy_router:compile([{'_',
+    [ {"/",             transee_index_handler, []}
+    , {"/api",          transee_api_handler,   []}
+    , {"/static/[...]", cowboy_static,         {dir, static_dir()}}
+    ]}]).
+
+static_dir() ->
+    filename:join([std:priv_dir(transee), "static"]). 
