64160ab64cd6e7d1ad6811e43257f3d424df2022
Yuri Artemev <i@artemeff.com>
2015-03-18 13:31:55 +0300

    enhance tests with real data from files

--- include/test.hrl
+++ include/test.hrl
@@ -1,23 +1,17 @@
 -include_lib("eunit/include/eunit.hrl").
 
--define(mock_http(Data), ((fun() ->
-    meck:new(httpc, [unstick, passthrough]),
-    meck:expect(httpc, request, fun(_,_,_,_) -> {ok, {s, h, Data}} end)
-end)())).
-
--define(mock_http_with_response(City, Filename), ((fun() ->
-    __Resp = ?load_sample_response(City, Filename),
-    ?mock_http(__Resp)
-end)())).
-
--define(unload_mock, meck:unload(httpc)).
-
--define(assert_json(JSON), json:from_binary(jsx:encode(JSON))).
--define(open_source(Mod), transee_worker:open_source(Mod)).
-
--define(load_sample_response(City, Filename), ((fun() ->
+-define(mock_http_with_response(City, Filename, Fun), ((fun() ->
     {ok, __CWD} = file:get_cwd(),
     __Path = filename:join([__CWD, "..", "test", "cities", City, Filename]),
     {ok, __Content} = file:read_file(__Path),
-    __Content
+    meck:new(httpc, [unstick, passthrough]),
+    meck:expect(httpc, request, fun(_,_,_,_) -> {ok, {s, h, __Content}} end),
+    try
+        Fun()
+    after
+        meck:unload(httpc)
+    end
 end)())).
+
+-define(assert_json(JSON), json:from_binary(jsx:encode(JSON))).
+-define(open_source(Mod), transee_worker:open_source(Mod)).
--- test/cities/transee_city_yaroslavl_test.erl
+++ test/cities/transee_city_yaroslavl_test.erl
@@ -27,39 +27,38 @@ test_transports() ->
     ?assertEqual(<<"2к"/utf8>>, json:get(<<"/0/items/1/name">>, JSON)).
 
 test_positions() ->
-    ?mock_http(<<"[['577','39.8811470','57.6100270','188','8','8','bus05.png']]">>),
-    Source = ?open_source(?MOD),
-    JSON = ?assert_json(?MOD:positions(Source)),
-    ?assertEqual(<<"577">>, json:get(<<"/0/items/0/items/0/gos_id">>, JSON)),
-    ?unload_mock.
+    ?mock_http_with_response(?MOD, "getpe.json", fun() ->
+        Source = ?open_source(?MOD),
+        JSON = ?assert_json(?MOD:positions(Source)),
+        ?assertEqual(<<"27">>, json:get(<<"/1/items/0/items/0/gos_id">>, JSON))
+    end).
 
 test_routes() ->
-    ?mock_http(<<"[['57.63','39.96'],['57.63','39.96']]">>),
-    Source = ?open_source(?MOD),
-    JSON = ?assert_json(?MOD:routes(Source)),
-    ?assertEqual(<<"2k">>, json:get(<<"/0/items/1/id">>, JSON)),
-    ?assertEqual([[57.63, 39.96], [57.63, 39.96]], json:get(<<"/0/items/1/route">>, JSON)),
-    ?unload_mock.
+    ?mock_http_with_response(?MOD, "getroute.json", fun() ->
+        Source = ?open_source(?MOD),
+        JSON = ?assert_json(?MOD:routes(Source)),
+        ?assertEqual(<<"2k">>, json:get(<<"/0/items/1/id">>, JSON)),
+        ?assertEqual([57.639400, 39.963450], hd(json:get(<<"/0/items/1/route">>, JSON)))
+    end).
 
 test_stations() ->
-    ?mock_http(<<"[['258','57.588407','39.845435',null],['259','57.585435','39.841287',null]]">>),
-    Source = ?open_source(?MOD),
-    JSON = ?assert_json(?MOD:stations(Source)),
-    ?assertEqual(<<"258">>, json:get(<<"/0/items/0/id">>, JSON)),
-    ?assertEqual([57.588407, 39.845435], json:get(<<"/0/items/0/position">>, JSON)),
-    ?unload_mock.
+    ?mock_http_with_response(?MOD, "getstations.json", fun() ->
+        Source = ?open_source(?MOD),
+        JSON = ?assert_json(?MOD:stations(Source)),
+        ?assertEqual(<<"258">>, json:get(<<"/0/items/0/id">>, JSON)),
+        ?assertEqual([57.588407, 39.845435], json:get(<<"/0/items/0/position">>, JSON))
+    end).
 
 test_station_info() ->
-    ?mock_http(<<"<b>Station \"Some Name\"</b><br>Au 19k. Ul. Some - Nothin.<br>Тб 1. Ярославль-Гл. - ЯШЗ"/utf8>>),
-    JSON = ?assert_json(?MOD:station_info(<<"808">>)),
-    ?assertEqual(<<"Station Some Name">>, json:get(<<"/name">>, JSON)),
-    ?assertEqual(<<"Au 19k. Ul. Some - Nothin.">>, json:get(<<"/transports/0">>, JSON)),
-    ?unload_mock.
+    ?mock_http_with_response(?MOD, "getstationinfo.html", fun() ->
+        JSON = ?assert_json(?MOD:station_info(<<"808">>)),
+        ?assertEqual(<<"Station Some Name">>, json:get(<<"/name">>, JSON)),
+        ?assertEqual(<<"Au 19k. Ul. Some - Nothin.">>, json:get(<<"/transports/0">>, JSON))
+    end).
 
 test_transport_info() ->
-    ?mock_http_with_response(?MOD, "getpeinfo.html"),
-    JSON = ?assert_json(?MOD:transport_info(<<"autobus">>, <<"123">>)),
-    ?assertEqual(<<"12:51">>, json:get(<<"/0/time">>, JSON)),
-    ?assertEqual(<<"13:19">>, json:get(<<"/13/time">>, JSON)),
-    ?unload_mock.
-
+    ?mock_http_with_response(?MOD, "getpeinfo.html", fun() ->
+        JSON = ?assert_json(?MOD:transport_info(<<"autobus">>, <<"123">>)),
+        ?assertEqual(<<"12:51">>, json:get(<<"/0/time">>, JSON)),
+        ?assertEqual(<<"13:19">>, json:get(<<"/13/time">>, JSON))
+    end).
