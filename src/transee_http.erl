61c48d66221a5ebee73c6c7105e3797b2675e34e
Yuri Artemev <i@artemeff.com>
2015-04-12 12:39:36 +0300

    fix transee_http
    * catch timeout errors
    * and retry when catched

--- src/transee_http.erl
+++ src/transee_http.erl
@@ -1,13 +1,19 @@
 -module(transee_http).
--export([request/3]).
+-export([request/3, request/4]).
 
 request(Method, URL, Headers) ->
-    HttpOptions = [{timeout, 1000}],
+    request(Method, URL, Headers, 10000).
+
+request(Method, URL, Headers, Timeout) ->
+    HttpOptions = [{timeout, Timeout}],
     Options = [{body_format, binary}],
     case httpc:request(Method, {URL, Headers}, HttpOptions, Options) of
         {ok, {_Status, _Headers, Body}} ->
             {ok, Body};
+        {error, timeout} ->
+            lager:error("transee_http timeout ~s ~s", [Method, URL]),
+            request(Method, URL, Headers, 20000);
         Error ->
-            lager:error("~p", [Error]),
+            lager:error("transee_http ~s ~s ~p", [Method, URL, Error]),
             undefined
     end.
