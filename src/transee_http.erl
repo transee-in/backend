9445912a99571289258ff59d4aa71b48daa56218
Yuri Artemev <i@artemeff.com>
2015-04-16 18:44:44 +0300

    add try_request_after for transee_http
    function simply sleeps process and retry request after timeout

--- src/transee_http.erl
+++ src/transee_http.erl
@@ -1,8 +1,10 @@
 -module(transee_http).
 -export([request/3, request/4]).
+-define(HTTP_REQUEST_TIMEOUT, 10000). % 10 seconds
+-define(PROCESS_SLEEP, 1000).         % 1 second
 
 request(Method, URL, Headers) ->
-    request(Method, URL, Headers, 10000).
+    request(Method, URL, Headers, ?HTTP_REQUEST_TIMEOUT).
 
 request(Method, URL, Headers, Timeout) ->
     HttpOptions = [{timeout, Timeout}],
@@ -11,9 +13,19 @@ request(Method, URL, Headers, Timeout) ->
         {ok, {_Status, _Headers, Body}} ->
             {ok, Body};
         {error, timeout} ->
-            lager:error("transee_http:request timeout ~s ~s", [Method, URL]),
-            request(Method, URL, Headers, 20000);
+            try_request_after(Method, URL, Headers, 20000);
+        {error, {failed_connect, _}} ->
+            try_request_after(Method, URL, Headers, 20000);
+        {error, socket_closed_remotely} ->
+            try_request_after(Method, URL, Headers);
         Error ->
             lager:error("transee_http:request error ~s ~s ~p", [Method, URL, Error]),
             undefined
     end.
+
+try_request_after(Method, URL, Headers) ->
+    try_request_after(Method, URL, Headers, ?HTTP_REQUEST_TIMEOUT).
+
+try_request_after(Method, URL, Headers, Timeout) ->
+    timer:sleep(?PROCESS_SLEEP),
+    request(Method, URL, Headers, Timeout).
